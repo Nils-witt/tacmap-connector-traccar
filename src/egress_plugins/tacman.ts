985ea0d7195a27616c3cf90fc6f6dcab9c8fbdb4
Nils Witt <nils.witt@nils-witt.de>
2026-04-09 17:32:56 +0200

    Enhance TacmanEgressPlugin: add support for username and password, implement token retrieval and retry logic

--- src/egress_plugins/tacman.ts
+++ src/egress_plugins/tacman.ts
@@ -5,7 +5,9 @@ import logger from '../logger';
 
 export default class TacmanEgressPlugin extends AbstractEgressPlugin {
   private url = '';
-  private token = '';
+  private token?: string;
+  private username?: string;
+  private password?: string;
 
   constructor() {
     super('tacman');
@@ -14,10 +16,18 @@ export default class TacmanEgressPlugin extends AbstractEgressPlugin {
 
   setConfig(config: Config) {
     try {
-      const pConf = config.plugins.egress[this.name] as unknown as { url: string; token: string };
+      const pConf = config.plugins.egress[this.name] as unknown as {
+        url: string;
+        token: string;
+        username: string;
+        password: string;
+      };
       logger.info('Plugin %s config: %o', this.name, pConf);
       this.url = pConf.url;
       this.token = pConf.token;
+      this.username = pConf.username;
+      this.password = pConf.password;
+      void this.getAuthToken();
     } catch (error) {
       logger.error('Error setting config for plugin %s', this.name, error);
     }
@@ -36,6 +46,36 @@ export default class TacmanEgressPlugin extends AbstractEgressPlugin {
     });
   }
 
+  async getAuthToken(): Promise<string | undefined> {
+    const payload = JSON.stringify({
+      username: this.username,
+      password: this.password,
+    });
+
+    console.log(payload);
+    const callUrl = `${this.url}/token`;
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: payload,
+    };
+    const res = await fetch(callUrl, options);
+    if (!res.ok) {
+      const errorText = await res.text();
+      logger.error(
+        'Failed to obtain auth token for Tacman, status: %s, response: %s',
+        res.status,
+        errorText,
+      );
+      return undefined;
+    } else {
+      const token = (await res.json()) as { token: string };
+      logger.info('Successfully obtained auth token for Tacman');
+      this.token = token.token;
+    }
+  }
   async sendToApi(
     unitId: string,
     latitude: number,
@@ -62,6 +102,26 @@ export default class TacmanEgressPlugin extends AbstractEgressPlugin {
     };
 
     const res = await fetch(callUrl, options);
-    logger.info(`Sent data to API for unit ${unitId}, response status: ${res.status}`);
+    if (!res.ok) {
+      const errorText = await res.text();
+      logger.error(
+        'Failed to send data to Tacman for unit %s, status: %s, response: %s',
+        unitId,
+        res.status,
+        errorText,
+      );
+      if (res.status === 401) {
+        logger.info('Obtained new auth token for Tacman, retrying request');
+
+        this.token = await this.getAuthToken();
+        console.log('New token: %s', this.token);
+        if (this.token) {
+          logger.info('Retrying to send data to Tacman for unit %s', unitId);
+          await this.sendToApi(unitId, latitude, longitude, altitude, accuracy);
+        }
+      }
+    } else {
+      logger.info('Successfully sent data to Tacman for unit %s, status: %s', unitId, res.status);
+    }
   }
 }
